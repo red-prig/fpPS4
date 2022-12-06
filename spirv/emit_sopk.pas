27aeca78671e1227ca4bffb10a860470a015dcf0
Pavel <68122101+red-prig@users.noreply.github.com>
2022-12-06 22:03:06 +0300

    SOPK?15

--- spirv/emit_sopk.pas
+++ spirv/emit_sopk.pas
@@ -15,16 +15,40 @@ type
  TEmit_SOPK=class(TEmitFetch)
   procedure emit_SOPK;
   procedure emit_S_MOVK_I32;
+  procedure emit_S_ADDK_I32;
  end;
 
 implementation
 
-procedure TEmit_SOPK.emit_S_MOVK_I32;
+function SignExtend16(W:Word):Integer; inline;
+const
+ shift=BitSizeOf(Integer)-BitSizeOf(Word);
+begin
+ Result:=SarLongint((Integer(W) shl shift),shift);
+end;
+
+procedure TEmit_SOPK.emit_S_MOVK_I32; //sdst.s = signExtend(imm16)
 Var
  dst:PsrRegSlot;
 begin
  dst:=get_sdst7(FSPI.SOPK.SDST);
- SetConst_i(dst,dtInt32,SmallInt(FSPI.SOPK.SIMM));
+ SetConst_i(dst,dtInt32,SignExtend16(FSPI.SOPK.SIMM));
+end;
+
+procedure TEmit_SOPK.emit_S_ADDK_I32; //sdst.s = (sdst.s + signExtend(imm16)); SCC = overflow
+Var
+ dst:PsrRegSlot;
+ car:PsrRegSlot;
+ src:PsrRegNode;
+ imm:PsrRegNode;
+begin
+ dst:=get_sdst7(FSPI.SOPK.SDST);
+ car:=get_scc;
+
+ src:=fetch_ssrc8(FSPI.SOPK.SDST,dtInt32);
+ imm:=NewReg_i(dtInt32,SignExtend16(FSPI.SOPK.SIMM));
+
+ OpIAddExt(dst,car,src,imm);
 end;
 
 procedure TEmit_SOPK.emit_SOPK;
@@ -33,6 +57,8 @@ begin
  Case FSPI.SOPK.OP of
 
   S_MOVK_I32: emit_S_MOVK_I32;
+
+  S_ADDK_I32: emit_S_ADDK_I32;
 
   else
    Assert(false,'SOPK?'+IntToStr(FSPI.SOPK.OP));
