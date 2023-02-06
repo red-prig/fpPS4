33f4bd4aa956dff6283b8b1a9b1e01a93244ae32
kagamma <kagamma.km@gmail.com>
2023-02-07 03:21:12 +0700

    Minor

--- src/ps4_libscehttp.pas
+++ src/ps4_libscehttp.pas
@@ -498,7 +498,9 @@ var
     PARSE_TYPE_HOSTNAME:
      begin
       _tokenExpected(token,[tkString,tkNumber]);
-      hostname:=token.value;
+       hostname:=token.value;
+      while _peekAtNextToken.kind in [tkString,tkNumber] do
+       hostname:=hostname+_nextToken.value;
       if _peekAtNextToken.kind=tkColon then
       begin
        _nextToken;
