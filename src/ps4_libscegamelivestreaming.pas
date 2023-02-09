cbcf47cac74f3f8f466d726a0d3fffa633444e25
Kagamma <kagamma.km@gmail.com>
2023-02-10 00:15:14 +0700

    videorecording + a few other funcs (#80)
    * libSceVideoRecording
    
    * sceGameLiveStreamingPermitServerSideRecording
    
    * sceSharePlaySetProhibition
    
    * sceGameLiveStreamingEnableLiveStreaming
    
    * sceGameLiveStreamingPermitLiveStreaming
    
    * fix types
    
    * LongBool->Boolean
    
    ---------
    
    Co-authored-by: red-prig <vdpasha@mail.ru>

--- src/ps4_libscegamelivestreaming.pas
+++ src/ps4_libscegamelivestreaming.pas
@@ -15,6 +15,24 @@ begin
  Result:=0;
 end;
 
+function ps4_sceGameLiveStreamingPermitServerSideRecording(isPermit:Boolean):Integer; SysV_ABI_CDecl;
+begin
+ Writeln('sceGameLiveStreamingPermitServerSideRecording,isPermit=',isPermit);
+ Result:=0;
+end;
+
+function ps4_sceGameLiveStreamingEnableLiveStreaming(isEnable:Boolean):Integer; SysV_ABI_CDecl;
+begin
+ Writeln('sceGameLiveStreamingEnableLiveStreaming,isEnable=',isEnable);
+ Result:=0;
+end;
+
+function ps4_sceGameLiveStreamingPermitLiveStreaming(isPermit:Boolean):Integer; SysV_ABI_CDecl;
+begin
+ Writeln('sceGameLiveStreamingPermitLiveStreaming,isPermit=',isPermit);
+ Result:=0;
+end;
+
 function Load_libSceGameLiveStreaming(Const name:RawByteString):TElf_node;
 var
  lib:PLIBRARY;
@@ -25,6 +43,9 @@ begin
  lib:=Result._add_lib('libSceGameLiveStreaming');
 
  lib^.set_proc($92F604C369419DD9,@ps4_sceGameLiveStreamingInitialize);
+ lib^.set_proc($FC41E753AF201315,@ps4_sceGameLiveStreamingPermitServerSideRecording);
+ lib^.set_proc($75633CD005F7F68E,@ps4_sceGameLiveStreamingEnableLiveStreaming);
+ lib^.set_proc($2B443111B0FBABE7,@ps4_sceGameLiveStreamingPermitLiveStreaming);
 end;
 
 initialization
