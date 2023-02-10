5508e914e807ac0b5960f8058ddf7f427cc2a03a
kagamma <kagamma.km@gmail.com>
2023-02-11 02:46:09 +0700

    sceAudiodecCpuDecode

--- src/ps4_libsceaudiodeccpu.pas
+++ src/ps4_libsceaudiodeccpu.pas
@@ -21,6 +21,22 @@ begin
  Result:=0;
 end;
 
+function ps4_sceAudiodecCpuInitDecoder(pCtrl      :PSceAudiodecCpuCtrl;
+                                       pRes       :PSceAudiodecCpuResource;
+                                       uiCodecType:DWord):Integer; SysV_ABI_CDecl;
+begin
+ Writeln('sceAudiodecCpuInitDecoder,uiCodecType=',uiCodecType);
+ Result:=0;
+end;
+
+function ps4_sceAudiodecCpuDecode(pCtrl      :PSceAudiodecCpuCtrl;
+                                  pRes       :PSceAudiodecCpuResource;
+                                  uiCodecType:DWord):Integer; SysV_ABI_CDecl;
+begin
+ // Writeln('sceAudiodecCpuDecode,uiCodecType=',uiCodecType);
+ Result:=0;
+end;
+
 function Load_libSceAudiodecCpu(Const name:RawByteString):TElf_node;
 var
  lib:PLIBRARY;
@@ -31,6 +47,8 @@ begin
  lib:=Result._add_lib('libSceAudiodecCpu');
 
  lib^.set_proc($92D0F6C370F81B65,@ps4_sceAudiodecCpuQueryMemSize);
+ lib^.set_proc($85D16CC68DCC16EF,@ps4_sceAudiodecCpuInitDecoder);
+ lib^.set_proc($952553896579C0B7,@ps4_sceAudiodecCpuDecode);
 end;
 
 initialization
