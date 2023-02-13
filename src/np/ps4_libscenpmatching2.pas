1ff3f04db6ce7abf2e1acfaa63e306b4848d958d
Kagamma <kagamma.km@gmail.com>
2023-02-13 16:25:22 +0700

    sceUsbdGetDeviceList + sceUsbdFreeDeviceList + sceUsbdHandleEventsTimeout + sceNpMatching2CreateContext (#84)
    * sceUsbdGetDeviceList + sceUsbdFreeDeviceList + sceUsbdHandleEventsTimeout
    
    * sceNpMatching2CreateContext
    
    * Declaration corrected
    
    ---------
    
    Co-authored-by: Pavel <68122101+red-prig@users.noreply.github.com>

--- src/np/ps4_libscenpmatching2.pas
+++ src/np/ps4_libscenpmatching2.pas
@@ -5,9 +5,13 @@ unit ps4_libSceNpMatching2;
 interface
 
 uses
-  ps4_program;
+  ps4_program,
+  ps4_libSceNpCommon;
 
 implementation
+
+const
+ SCE_NP_MATCHING2_ERROR_NOT_INITIALIZED=$80550c03;
 
 type
  pSceNpMatching2InitializeParameter=^SceNpMatching2InitializeParameter;
@@ -19,6 +23,18 @@ type
   threadStackSize:QWORD;  // 0 = default
   size:QWORD;             // size of this structure
   sslPoolSize:QWORD;      // 0 = default
+ end;
+
+ SceNpMatching2ContextId=Word;
+
+ pSceNpMatching2CreateContextParam=^SceNpMatching2CreateContextParam;
+ SceNpMatching2CreateContextParam=packed record
+  npId        :pSceNpId;
+  commId      :pSceNpCommunicationId;
+  passPhrase  :pSceNpCommunicationPassphrase;
+  serviceLabel:SceNpServiceLabel;
+  _align      :DWORD;
+  size        :size_t;
  end;
 
  SceNpMatching2ContextCallback=procedure(
@@ -38,6 +54,13 @@ begin
  Result:=0;
 end;
 
+function ps4_sceNpMatching2CreateContext(param:pSceNpMatching2CreateContextParam;
+                                         ctxId:PWord):Integer; SysV_ABI_CDecl;
+begin
+ Writeln('sceNpMatching2CreateContext,serviceLabel=',param^.serviceLabel,',size=',param^.size);
+ Result:=SCE_NP_MATCHING2_ERROR_NOT_INITIALIZED;
+end;
+
 function Load_libSceNpMatching2(Const name:RawByteString):TElf_node;
 var
  lib:PLIBRARY;
@@ -48,6 +71,7 @@ begin
  lib:=Result._add_lib('libSceNpMatching2');
  lib^.set_proc($D74B777B9F893E75,@ps4_sceNpMatching2Initialize);
  lib^.set_proc($7D041F3FCEC8EE1B,@ps4_sceNpMatching2RegisterContextCallback);
+ lib^.set_proc($61F9A95BBD7DACCA,@ps4_sceNpMatching2CreateContext);
 end;
 
 initialization
