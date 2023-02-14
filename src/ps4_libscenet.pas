21c9d0f9d86eff69f6277e7b861740b42a7e6b40
Addy2004 <40354792+Addy2004@users.noreply.github.com>
2023-02-14 15:01:15 +0530

    sceNetEpollDestroy

--- src/ps4_libscenet.pas
+++ src/ps4_libscenet.pas
@@ -345,6 +345,11 @@ begin
  Result:=0;
 end;
 
+function ps4_sceNetEpollDestroy(eid:Integer):Integer; SysV_ABI_CDecl;
+begin
+ Result:=0;
+end;
+
 function ps4_sceNetCtlInit:Integer; SysV_ABI_CDecl;
 begin
  Result:=0;
@@ -626,6 +631,7 @@ begin
  lib^.set_proc($9099581F9B8C0162,@ps4_sceNetResolverDestroy);
  lib^.set_proc($3975D7E26524DEE9,@ps4_sceNetConnect);
  lib^.set_proc($76B8C86C36C0ED44,@ps4_sceNetEpollWait);
+ lib^.set_proc($227A7595F2FE25DC,@ps4_sceNetEpollDestroy);
 end;
 
 function Load_libSceNetCtl(Const name:RawByteString):TElf_node;
