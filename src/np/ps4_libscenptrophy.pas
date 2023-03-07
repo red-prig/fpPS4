54b6dd57674f3205056d56a26ee54027b0d50128
Pavel <68122101+red-prig@users.noreply.github.com>
2023-03-07 13:47:44 +0300

    sceNpTrophyAbortHandle

--- src/np/ps4_libscenptrophy.pas
+++ src/np/ps4_libscenptrophy.pas
@@ -154,6 +154,11 @@ begin
  Result:=0;
 end;
 
+function ps4_sceNpTrophyAbortHandle(handle:Integer):Integer; SysV_ABI_CDecl;
+begin
+ Result:=0;
+end;
+
 function ps4_sceNpTrophyRegisterContext(context:Integer;
                                         handle :Integer;
                                         options:QWORD):Integer; SysV_ABI_CDecl;
@@ -323,6 +328,7 @@ begin
  lib^.set_proc($5DB9236E86D99426,@ps4_sceNpTrophyCreateContext);
  lib^.set_proc($ABB53AB440107FB7,@ps4_sceNpTrophyCreateHandle);
  lib^.set_proc($18D705E2889D6346,@ps4_sceNpTrophyDestroyHandle);
+ lib^.set_proc($6939C7B3B5BFF549,@ps4_sceNpTrophyAbortHandle);
  lib^.set_proc($4C9080C6DA3D4845,@ps4_sceNpTrophyRegisterContext);
  lib^.set_proc($2C7B9298EDD22DDF,@ps4_sceNpTrophyGetTrophyUnlockState);
  lib^.set_proc($DBCC6645415AA3AF,@ps4_sceNpTrophyUnlockTrophy);
