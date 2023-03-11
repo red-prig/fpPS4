104bb77844045b273eeb4ac1257db0a602aa48c2
red-prig <vdpasha@mail.ru>
2023-03-11 16:04:01 +0300

    fix wrong declared sceNpCreateEventFlag

--- src/np/ps4_libscenpcommon.pas
+++ src/np/ps4_libscenpcommon.pas
@@ -217,13 +217,14 @@ begin
  end;
 end;
 
-function ps4_sceNpCreateEventFlag(ef:SceKernelEventFlag;
-                                  bitPattern:QWORD;
-                                  waitMode:DWORD;
-                                  pResultPat:PQWORD):Integer; SysV_ABI_CDecl;
+function ps4_sceNpCreateEventFlag(ef:pSceKernelEventFlag;
+                                  pName:PChar;
+                                  attr:DWORD;
+                                  initPattern:QWORD
+                                  ):Integer; SysV_ABI_CDecl;
 begin
- Result:=ps4_sceKernelWaitEventFlag(ef,bitPattern,waitMode,pResultPat,nil);
- Result:=Result shr $1F and Result; // Looks like bool, but True when Result<0
+ Result:=ps4_sceKernelCreateEventFlag(ef,pName,attr,initPattern,nil);
+ Result:=(Result shr $1F) and Result; // Looks like bool, but True when Result<0
 end;
 
 // sce::np::Object::operator new(unsigned long, SceNpAllocator&)
