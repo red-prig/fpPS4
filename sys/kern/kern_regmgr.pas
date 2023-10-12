7105f08f25b88eee85bd9ad472819f22ad2f2d0f
Pavel <68122101+red-prig@users.noreply.github.com>
2023-10-12 13:35:47 +0300

    +

--- sys/kern/kern_regmgr.pas
+++ sys/kern/kern_regmgr.pas
@@ -6,11 +6,19 @@ unit kern_regmgr;
 interface
 
 const
- SCE_REGMGR_ENT_KEY_NP_debug                   =$19810000;
- SCE_REGMGR_ENT_KEY_BROWSER_DEBUG_notification =$3CC80700;
- SCE_REGMGR_ENT_KEY_DEVENV_TOOL_trc_notify     =$78026400;
- SCE_REGMGR_ENT_KEY_DEVENV_TOOL_sys_prx_preload=$78028A00;
- SCE_REGMGR_ENT_KEY_DEVENV_TOOL_use_default_lib=$78028300;
+ SCE_REGMGR_ENT_KEY_NP_env                      = $19800000;
+ SCE_REGMGR_ENT_KEY_NP_debug                    = $19810000;
+ SCE_REGMGR_ENT_KEY_BROWSER_DEBUG_notification  = $3CC80700;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_trc_notify      = $78026400;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_sys_prx_preload = $78028A00;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_use_default_lib = $78028300;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_game_intmem_dbg = $7802BF00;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_sce_module_dbg  = $7802C000;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_preload_chk_off = $78020500;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_020B00          = $78020B00;
+ SCE_REGMGR_ENT_KEY_MORPHEUS_DEBUG_vr_capture   = $58800C00;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_game_heap_trace = $7802B700;
+ SCE_REGMGR_ENT_KEY_DEVENV_TOOL_expose_under_2k = $7802B900;
 
 function sys_regmgr_call(op,key:DWORD;presult,pvalue:Pointer;vlen:QWORD):Integer;
 function sys_workaround8849(key:DWORD):Integer;
@@ -23,6 +31,21 @@ uses
  kern_thr,
  trap;
 
+const
+ s_ScrambNSsdk:array[0..15] of Byte=(
+  $6B, $E8, $98, $03,
+  $9A, $70, $23, $5A,
+  $63, $EE, $F5, $7B,
+  $FF, $A4, $4C, $8C
+ );
+
+ s_ScrambNSlib:array[0..15] of Byte=(
+  $14, $EE, $DE, $E1,
+  $80, $AC, $F3, $78,
+  $47, $43, $DB, $40,
+  $93, $DD, $B1, $34
+ );
+
 type
  t_encoded_id=packed record
   data    :array[0..3] of Byte;
@@ -31,15 +54,67 @@ type
   checksum:Word;
  end;
 
+function regMgrCnvRegId(enc:QWORD):Integer;
+var
+ a,b,c,d,e,f,x:Byte;
+ g:DWORD;
+ h:DWORD;
+ y:DWORD;
+ p_Scramb:PByte;
+ data:array[0..3] of Byte;
+begin
+ Result:=0;
+
+ a:=enc and $ff;            //data[0]
+ b:=(enc shr  8) and $ff;   //data[1]
+ c:=(enc shr 16) and $ff;   //data[2]
+ d:=(enc shr 24) and $ff;   //data[3]
+ e:=(enc shr 32) and $ff;   //table
+ f:=(enc shr 40) and $ff;   //index
+ g:=(enc shr 48) and $ffff; //checksum
+ h:=(a + b + c + d + e * f) and $ffff;
+
+ if (g<>h) then Exit(Integer($800d0204));
+
+ f:=f xor $6b;
+
+ if (f > 12) then Exit(Integer($800d0203));
+
+ x:=e xor s_ScrambNSsdk[$f - f];
+
+ if (x=$19) then
+ begin
+  p_Scramb:=@s_ScrambNSlib;
+ end else
+ begin
+  p_Scramb:=@s_ScrambNSsdk;
+  if (x<>$72) then Exit(Integer($800d0205));
+ end;
+
+ x:=p_Scramb[f];
+ y:=p_Scramb[f + 1];
+
+ data[0]:=(a xor p_Scramb[f + 3]);
+ data[1]:=(c xor p_Scramb[f + 2]);
+ data[2]:=(d xor y);
+ data[3]:=(b xor x);
+
+ Result:=(data[0]       ) or
+         (data[1] shl  8) or
+         (data[2] shl 16) or
+         (data[3] shl 24)
+end;
+
 function sys_regmgr_call(op,key:DWORD;presult,pvalue:Pointer;vlen:QWORD):Integer;
 label
  _copyout_value,
  _err;
 var
  kret:DWORD;
+ skey:Integer;
 
  data:packed record
-  enc :t_encoded_id;
+  enc :QWORD;
   val1:DWORD;
   val2:DWORD;
   slen:DWORD;
@@ -66,18 +141,26 @@ begin
         goto _err;
        end;
 
-       case qword(data.enc) of
-        QWORD($0C82671ADF0EEB34):data.val2:=0; //_malloc_init_lv2
-        QWORD($1AC46343411B3F40):data.val2:=0; //libSceSysmodule  (bit 1,2 -> load debug lib)
-        QWORD($503F69BDE385A6AC):data.val2:=0; //libSceSysmodule  (print errors?)
-        QWORD($2D946F62AEF8F878):data.val2:=0; //libSceSysmodule  (preload module?)
-        QWORD($1BE26343C3D71F40):data.val2:=0; //libkernel
-        QWORD($68436EECF1CFD447):data.val2:=0; //libSceLibcInternal (sceLibcHeapGetTraceInfo -> get_segment_info)
-        QWORD($29B56169422AA3DD):data.val2:=0; //sceVideoOutOpen  debug video modes?
+       skey:=regMgrCnvRegId(data.enc);
+
+       if (skey<0) then
+       begin
+        kret:=skey;
+        goto _err;
+       end;
+
+       case skey of
+        SCE_REGMGR_ENT_KEY_DEVENV_TOOL_game_intmem_dbg:data.val2:=0; //_malloc_init_lv2
+        SCE_REGMGR_ENT_KEY_DEVENV_TOOL_sce_module_dbg :data.val2:=0; //libSceSysmodule  (bit 1,2 -> load debug lib)
+        SCE_REGMGR_ENT_KEY_DEVENV_TOOL_preload_chk_off:data.val2:=0; //libSceSysmodule  (print errors?)
+        SCE_REGMGR_ENT_KEY_DEVENV_TOOL_020B00         :data.val2:=0; //libSceSysmodule  (preload module?)
+        SCE_REGMGR_ENT_KEY_MORPHEUS_DEBUG_vr_capture  :data.val2:=0; //libkernel
+        SCE_REGMGR_ENT_KEY_DEVENV_TOOL_game_heap_trace:data.val2:=0; //libSceLibcInternal (sceLibcHeapGetTraceInfo -> get_segment_info)
+        SCE_REGMGR_ENT_KEY_DEVENV_TOOL_expose_under_2k:data.val2:=0; //sceVideoOutOpen  debug video modes?
 
         else
          begin
-          Writeln(' enc:0x',HexStr(qword(data.enc),16));
+          Writeln(' enc:0x',HexStr(data.enc,16),'->key:0x',HexStr(skey,8));
           print_backtrace_td(stderr);
           Assert(False);
          end;
@@ -101,14 +184,22 @@ begin
         goto _err;
        end;
 
-       case qword(data.enc) of
-        QWORD($6B976DF7F847EA43): //sceNpUtilGetEnv
+       skey:=regMgrCnvRegId(data.enc);
+
+       if (skey<0) then
+       begin
+        kret:=skey;
+        goto _err;
+       end;
+
+       case skey of
+        SCE_REGMGR_ENT_KEY_NP_env: //sceNpUtilGetEnv
           begin
            data.dstr:='np';
           end;
         else
          begin
-          Writeln(' enc:0x',HexStr(qword(data.enc),16));
+          Writeln(' enc:0x',HexStr(data.enc,16),'->key:0x',HexStr(skey,8));
           print_backtrace_td(stderr);
           Assert(False);
          end;
