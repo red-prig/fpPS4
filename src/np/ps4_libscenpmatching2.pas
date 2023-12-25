6c096f21010e0ec67d2130b2d86be7ef68aaa889
Ordinary205 <125112932+Ordinary205@users.noreply.github.com>
2023-12-25 13:44:30 +0000

    sceNpMatching2CreateContextA

--- src/np/ps4_libscenpmatching2.pas
+++ src/np/ps4_libscenpmatching2.pas
@@ -60,6 +60,11 @@ begin
  Result:=SCE_NP_MATCHING2_ERROR_NOT_INITIALIZED;
 end;
 
+function ps4_sceNpMatching2CreateContextA(ctxId:SceNpMatching2ContextId):Integer; SysV_ABI_CDecl;
+begin
+ Result:=0;
+end;
+
 function ps4_sceNpMatching2Terminate():Integer; SysV_ABI_CDecl;
 begin
  Result:=0;
@@ -76,6 +81,7 @@ begin
  lib^.set_proc($D74B777B9F893E75,@ps4_sceNpMatching2Initialize);
  lib^.set_proc($7D041F3FCEC8EE1B,@ps4_sceNpMatching2RegisterContextCallback);
  lib^.set_proc($61F9A95BBD7DACCA,@ps4_sceNpMatching2CreateContext);
+ lib^.set_proc($6A3BF373C7B6BA9A,@ps4_sceNpMatching2CreateContextA);
  lib^.set_proc($32AA77949FAC8F2E,@ps4_sceNpMatching2Terminate);
 end;
 
