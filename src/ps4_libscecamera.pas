6be361dcd64578aeda36196e052ca2d5f5aeccf3
krispy <gluchydejf@gmail.com>
2024-01-15 19:26:56 +0100

    sceCameraOpen function

--- src/ps4_libscecamera.pas
+++ src/ps4_libscecamera.pas
@@ -18,6 +18,11 @@ begin
  Result:=0;
 end;
 
+function ps4_sceCameraOpen(userId:Integer;_type,index:Integer;pParam:PInteger):Integer; SysV_ABI_CDecl;
+begin
+ Result:=1;
+end;
+
 function Load_libSceCamera(Const name:RawByteString):TElf_node;
 var
  lib:PLIBRARY;
@@ -26,6 +31,7 @@ begin
  Result.pFileName:=name;
  lib:=Result._add_lib('libSceCamera');
  lib^.set_proc($A7A9F73698B7618E,@ps4_sceCameraIsAttached);
+ lib^.set_proc($0479FCDF1AC5F761,@ps4_sceCameraOpen);
 end;
 
 initialization
