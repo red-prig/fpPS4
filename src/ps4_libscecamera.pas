4460e17647e08e53e66cf6c5727b2054b73b5ac7
krispy <125815038+kr1spy2115@users.noreply.github.com>
2024-01-16 17:42:51 +0100

    sceCameraOpen function (#171)
    * sceCameraOpen function
    
    * smoll fix
    
    * +
    
    ---------
    
    Co-authored-by: red-prig <vdpasha@mail.ru>

--- src/ps4_libscecamera.pas
+++ src/ps4_libscecamera.pas
@@ -12,10 +12,23 @@ uses
 
 implementation
 
+type
+ pSceCameraOpenParameter=^SceCameraOpenParameter;
+ SceCameraOpenParameter=packed record
+  sizeThis :DWORD;
+  reserved1:DWORD;
+  reserved2:DWORD;
+  reserved3:DWORD;
+ end;
 
 function ps4_sceCameraIsAttached(index:Integer):Integer; SysV_ABI_CDecl;
 begin
  Result:=0;
+end;
+
+function ps4_sceCameraOpen(userId,_type,index:Integer;pParam:pSceCameraOpenParameter):Integer; SysV_ABI_CDecl;
+begin
+ Result:=333;
 end;
 
 function Load_libSceCamera(Const name:RawByteString):TElf_node;
@@ -26,6 +39,7 @@ begin
  Result.pFileName:=name;
  lib:=Result._add_lib('libSceCamera');
  lib^.set_proc($A7A9F73698B7618E,@ps4_sceCameraIsAttached);
+ lib^.set_proc($0479FCDF1AC5F761,@ps4_sceCameraOpen);
 end;
 
 initialization
