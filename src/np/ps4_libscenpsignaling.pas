9d25fed1dc8a25c5b78f9422486cf569cdd110d3
Ordinary205 <125112932+Ordinary205@users.noreply.github.com>
2024-02-01 21:56:19 +0400

    sceNpSignalingCreateContext

--- src/np/ps4_libscenpsignaling.pas
+++ src/np/ps4_libscenpsignaling.pas
@@ -7,12 +7,32 @@ interface
 uses
   ps4_program;
 
+const
+ SCE_NP_SIGNALING_CONTEXT_MAX=8;
+
+type
+ pSceNpSignalingHandler=^SceNpSignalingHandler;
+ SceNpSignalingHandler=packed record
+  ctxId    :DWORD;
+  subjectId:DWORD;
+  event    :Integer;
+  errorCode:Integer;
+  arg      :Pointer;
+ end;
+
 implementation
 
 function ps4_sceNpSignalingInitialize(poolSize:QWORD;
                                       threadPriority:Integer;
                                       cpuAffinityMask:Integer;
                                       threadStackSize:QWORD):Integer; SysV_ABI_CDecl;
+begin
+ Result:=0;
+end;
+
+function ps4_sceNpSignalingCreateContext(userId,handler:SceNpSignalingHandler;
+                                         arg:Pointer;
+                                         ctxId:PQWORD):Integer; SysV_ABI_CDecl;
 begin
  Result:=0;
 end;
@@ -26,6 +46,7 @@ begin
 
  lib:=Result._add_lib('libSceNpSignaling');
  lib^.set_proc($DCA3AE0B84666595,@ps4_sceNpSignalingInitialize);
+ lib^.set_proc($E7262311D778B7C6,@ps4_sceNpSignalingCreateContext)
 end;
 
 initialization
