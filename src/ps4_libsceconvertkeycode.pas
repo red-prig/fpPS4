3381f8e7fda5ecee9d3d22275386e9440e2d68e2
red-prig <vdpasha@mail.ru>
2024-02-02 20:33:07 +0300

    +

--- src/ps4_libsceconvertkeycode.pas
+++ src/ps4_libsceconvertkeycode.pas
@@ -6,57 +6,14 @@ interface
 
 uses
   ps4_program,
+  ps4_libSceIme,
   Classes,
   SysUtils;
-
-const
- SCE_IME_KEYBOARD_TYPE_NONE            =0;
- SCE_IME_KEYBOARD_TYPE_DANISH          =1;
- SCE_IME_KEYBOARD_TYPE_GERMAN          =2;
- SCE_IME_KEYBOARD_TYPE_GERMAN_SW       =3;
- SCE_IME_KEYBOARD_TYPE_ENGLISH_US      =4;
- SCE_IME_KEYBOARD_TYPE_ENGLISH_GB      =5;
- SCE_IME_KEYBOARD_TYPE_SPANISH         =6;
- SCE_IME_KEYBOARD_TYPE_SPANISH_LA      =7;
- SCE_IME_KEYBOARD_TYPE_FINNISH         =8;
- SCE_IME_KEYBOARD_TYPE_FRENCH          =9;
- SCE_IME_KEYBOARD_TYPE_FRENCH_BR       =10;
- SCE_IME_KEYBOARD_TYPE_FRENCH_CA       =11;
- SCE_IME_KEYBOARD_TYPE_FRENCH_SW       =12;
- SCE_IME_KEYBOARD_TYPE_ITALIAN         =13;
- SCE_IME_KEYBOARD_TYPE_DUTCH           =14;
- SCE_IME_KEYBOARD_TYPE_NORWEGIAN       =15;
- SCE_IME_KEYBOARD_TYPE_POLISH          =16;
- SCE_IME_KEYBOARD_TYPE_PORTUGUESE_BR   =17;
- SCE_IME_KEYBOARD_TYPE_PORTUGUESE_PT   =18;
- SCE_IME_KEYBOARD_TYPE_RUSSIAN         =19;
- SCE_IME_KEYBOARD_TYPE_SWEDISH         =20;
- SCE_IME_KEYBOARD_TYPE_TURKISH         =21;
- SCE_IME_KEYBOARD_TYPE_JAPANESE_ROMAN  =22;
- SCE_IME_KEYBOARD_TYPE_JAPANESE_KANA   =23;
- SCE_IME_KEYBOARD_TYPE_KOREAN          =24;
- SCE_IME_KEYBOARD_TYPE_SM_CHINESE      =25;
- SCE_IME_KEYBOARD_TYPE_TR_CHINESE_ZY   =26;
- SCE_IME_KEYBOARD_TYPE_TR_CHINESE_PY_HK=27;
- SCE_IME_KEYBOARD_TYPE_TR_CHINESE_PY_TW=28;
- SCE_IME_KEYBOARD_TYPE_TR_CHINESE_CG   =29;
- SCE_IME_KEYBOARD_TYPE_ARABIC_AR       =30;
- SCE_IME_KEYBOARD_TYPE_THAI            =31;
- SCE_IME_KEYBOARD_TYPE_CZECH           =32;
- SCE_IME_KEYBOARD_TYPE_GREEK           =33;
- SCE_IME_KEYBOARD_TYPE_INDONESIAN      =34;
- SCE_IME_KEYBOARD_TYPE_VIETNAMESE      =35;
- SCE_IME_KEYBOARD_TYPE_ROMANIAN        =36;
- SCE_IME_KEYBOARD_TYPE_HUNGARIAN       =37;
-
-type
- pSceImeKeyboardType=^SceImeKeyboardType;
- SceImeKeyboardType=Integer;
 
 implementation
 
 function ps4_sceConvertKeycodeGetVirtualKeycode(keycode:Word;
-                                                keyboardType:SceImeKeyboardType;
+                                                keyboardType:Integer;
                                                 vkeycode:PWord):Integer; SysV_ABI_CDecl;
 begin
  Result:=0;
@@ -74,7 +31,8 @@ begin
 end;
 
 initialization
- ps4_app.RegistredPreLoad('libSceConvertKeycode.prx',@Load_libSceConvertKeycode);
+ //low priority
+ ps4_app.RegistredFinLoad('libSceConvertKeycode.prx',@Load_libSceConvertKeycode);
 
 end.
 
