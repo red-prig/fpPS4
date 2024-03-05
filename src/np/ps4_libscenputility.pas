8615d6215a76b2321d47383ed071c1e751bcd0c4
Ordinary205 <125112932+Ordinary205@users.noreply.github.com>
2024-03-05 21:30:40 +0400

    SceNpBandwidthTestInitStart

--- src/np/ps4_libscenputility.pas
+++ src/np/ps4_libscenputility.pas
@@ -11,6 +11,16 @@ uses
 const
  SCE_NP_LOOKUP_MAX_CTX_NUM=32;
 
+type
+ pSceNpBandwidthTestInitParam=^SceNpBandwidthTestInitParam;
+ SceNpBandwidthTestInitParam=packed record
+  size           :QWORD;
+  threadPriority :Integer;
+  padding        :array[0..3] of char;
+  cpuAffinityMask:QWORD;
+  reserved       :array[0..7] of DWORD;
+ end;
+
 implementation
 
 function ps4_sceNpLookupCreateTitleCtx(selfNpId:PSceNpId):Integer; SysV_ABI_CDecl;
@@ -23,6 +33,11 @@ begin
  Result:=1;
 end;
 
+function ps4_sceNpBandwidthTestInitStart(const param:pSceNpBandwidthTestInitParam):Integer; SysV_ABI_CDecl;
+begin
+ Result:=0;
+end;
+
 function Load_libSceNpUtility(Const name:RawByteString):TElf_node;
 var
  lib:PLIBRARY;
@@ -33,6 +48,7 @@ begin
  lib:=Result._add_lib('libSceNpUtility');
  lib^.set_proc($F39DF743E2D4EC44,@ps4_sceNpLookupCreateTitleCtx);
  lib^.set_proc($BD3F7186A3CEEBED,@ps4_sceNpLookupCreateTitleCtxA);
+ lib^.set_proc($8E4B70C37C895E77,@ps4_sceNpBandwidthTestInitStart);
  ///lib^.set_proc($E7262311D778B7C6,@ps4_sceNpSignalingCreateContext);
 end;
 
