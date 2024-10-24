3fe4a95a8a4e6be9701d553e662518d413bf184d
Pavel <68122101+red-prig@users.noreply.github.com>
2024-10-24 23:45:37 +0300

    +

--- src/ps4_libsceavsetting.pas
+++ src/ps4_libsceavsetting.pas
@@ -56,6 +56,43 @@ begin
  Result:=0;
 end;
 
+type
+ pAvDeviceInfo=^TAvDeviceInfo;
+ TAvDeviceInfo=packed record
+  size       :DWORD;
+  unknow_0x04:DWORD;
+  unknow_0x08:QWORD;
+  unknow_0x10:Byte;
+  _align1    :array[0..2] of Byte;
+  unknow_0x14:Single;
+  unknow_0x18:QWORD;
+  capability :QWORD;
+ end;
+ {$IF sizeof(TAvDeviceInfo)<>40}{$STOP sizeof(TAvDeviceInfo)<>40}{$ENDIF}
+
+
+function ps4_sceAvSettingGetDeviceInfo(
+          param_1:QWORD; //0x700
+          pInfo  :pAvDeviceInfo
+         ):Integer;
+begin
+ if (pInfo=nil) then
+ begin
+  Exit(Integer($809a0002));
+ end;
+
+ if (param_1<>$7000) then
+ begin
+  Exit(Integer($809a0003));
+ end;
+
+ pInfo^:=Default(TAvDeviceInfo);
+ pInfo^.size       :=40;
+ pInfo^.unknow_0x04:=1;
+
+ Result:=0;
+end;
+
 //
 
 function Load_libSceAvSetting(name:pchar):p_lib_info;
@@ -67,6 +104,7 @@ begin
  lib:=Result^.add_lib('libSceAvSetting');
  lib.set_proc($10865D5934FB65DC,@ps4_sceAvSettingInit);
  lib.set_proc($65F3078150D8CF36,@ps4_sceAvSettingChangeOutputMode3);
+ lib.set_proc($BE1DDA43E254A525,@ps4_sceAvSettingGetDeviceInfo);
 end;
 
 var
