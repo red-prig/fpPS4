bb88f6428a0d9a87dbe6eace2e14891a339d36b0
Pavel <68122101+red-prig@users.noreply.github.com>
2025-07-15 23:11:35 +0300

    +

--- src/ps4_libsceshareutility.pas
+++ src/ps4_libsceshareutility.pas
@@ -13,6 +13,12 @@ const
 
 implementation
 
+function ps4_sceShareUtilityInitialize(functionFlag:QWORD;
+                                       heapSize:QWORD):Integer;
+begin
+ Result:=0;
+end;
+
 function ps4_sceShareUtilityInitializeEx2(functionFlag:QWORD;
                                           heapSize:QWORD;
                                           threadPriority:Integer;
@@ -42,6 +48,7 @@ begin
  Result:=obj_new_int('libSceShareUtility');
 
  lib:=Result^.add_lib('libSceShareUtility');
+ lib.set_proc($265BFF958C449EF3,@ps4_sceShareUtilityInitialize);
  lib.set_proc($8FB0E56A50731E1F,@ps4_sceShareUtilityInitializeEx2);
  lib.set_proc($BB86C21A4AA1381E,@ps4_sceShareUtilityAdvanceInitialize);
  lib.set_proc($91AAE72616A474A8,@ps4_sceShareUtilityAdvanceSetUploadContentData);
