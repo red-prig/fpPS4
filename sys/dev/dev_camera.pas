71b20fccf917abb9f3a76bbacf550e2bfcca9da0
Pavel <68122101+red-prig@users.noreply.github.com>
2025-10-25 21:58:40 +0300

    +

--- sys/dev/dev_camera.pas
+++ sys/dev/dev_camera.pas
@@ -13,7 +13,8 @@ procedure camera_init();
 implementation
 
 uses
- errno;
+ errno,
+ vm;
 
 Function camera_ioctl(dev:p_cdev;cmd:QWORD;data:Pointer;fflag:Integer):Integer;
 begin
@@ -21,7 +22,31 @@ begin
 
  Writeln('camera_ioctl(0x',HexStr(cmd,8),')');
 
- Assert(false);
+ case cmd of
+
+  $40408E22: //mmap config?
+    begin
+     PDWORD(data)[0]:=0;
+     PDWORD(data)[1]:=0;
+
+     //if sceSblACMgrIsShelluiProcess(*) {
+     //  *(byte *)data = *(byte *)data | 1;
+     //}
+
+    end;
+
+   //$c0288e14:; //sceCameraDevOpen
+
+  else
+   Assert(false);
+ end;
+
+end;
+
+Function camera_mmap(dev:p_cdev;offset:vm_ooffset_t;paddr:p_vm_paddr_t;nprot:Integer;memattr:p_vm_memattr_t):Integer;
+begin
+ Writeln('TODO:camera_mmap');
+ Exit(EINVAL);
 end;
 
 const
@@ -36,7 +61,7 @@ const
   d_write       :nil;
   d_ioctl       :@camera_ioctl;
   d_poll        :nil;
-  d_mmap        :nil;
+  d_mmap        :@camera_mmap;
   d_strategy    :nil;
   d_dump        :nil;
   d_kqfilter    :nil;
