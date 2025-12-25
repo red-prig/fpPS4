abdc3a9e33a7df7a0b3ab29035855a6642864cc3
Pavel <68122101+red-prig@users.noreply.github.com>
2025-12-25 19:13:42 +0300

    vpblendd

--- sys/jit/kern_jit_ops_avx.pas
+++ sys/jit/kern_jit_ops_avx.pas
@@ -1322,6 +1322,8 @@ begin
  jit_cbs[OPPnone,OPvpbroadcast,OPSx_i128]:=@op_invalid;
  jit_cbs[OPPnone,OPvpbroadcast,OPSx_q   ]:=@op_invalid;
  jit_cbs[OPPnone,OPvpbroadcast,OPSx_w   ]:=@op_invalid;
+
+ jit_cbs[OPPv,OPpblend,OPSx_d           ]:=@op_invalid;
  //AVX2
 
  jit_cbs[OPPv,OPpextr,OPSx_b]:=@op_avx3_mri;
